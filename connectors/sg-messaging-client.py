54c167763bc23118cc7976733b1b50bcb51bb697
Mickael van der Beek <mickael@sagacify.com>
2014-06-23 16:47:14 +0200

    Added the Python connector and the related config on the Node.js class. Testing now.

---
+++ connectors/sg-messaging-client.py
@@ -0,0 +1,24 @@
+import zerorpc
+
+class sgMessagingClient (object):
+
+	def __init__ (self, commands):
+		commandNames = commands.keys()
+		commands['COMMAND_LIST'] = self.provideCommandList(commandNames)
+
+		server = zerorpc.Server(commands)
+		server.bind('tcp://127.0.0.1:4242')
+		server.run()
+
+	def provideCommandList (self, commandNames):
+		commands = {}
+		commandName = None
+
+		for i in range(len(commandNames)):
+			commandName = commandNames[i]
+			commands[commandName] = {}
+
+		def responseCommandList ():
+			return commands
+
+		return responseCommandList
